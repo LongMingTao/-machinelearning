c1ae32a4e37777d37243261290e1eea307057b9c
longmingtao <mingtao.long@hobot.cc>
2017-12-15 21:37:26 +0800

    it works

--- linearegression.py
+++ linearegression.py
@@ -3,6 +3,7 @@ import random
 
 
 def feature_scaling(train_x, mode=None):
+    train_x = train_x.copy()
     if not mode or mode == 'mean_normalization':
         for n in range(train_x.shape[1]):
             col_max = train_x[:, n].max()
@@ -13,51 +14,85 @@ def feature_scaling(train_x, mode=None):
                 train_x[m, n] = (train_x[m, n] - col_avg) / col_max
     else:
         pass
-    print(train_x)
     return train_x
 
 
-def gradient_descent(train_x, train_y, alpha):
-    train_x = feature_scaling(train_x)
+def gradient_descent(train_x, train_y, alpha, is_fs=True, show_detail=False):
+    if is_fs:
+        pre_x = train_x
+        train_x = feature_scaling(train_x)
+    else:
+        pass
     if train_x.shape[0] != train_y.shape[0]:
         raise Exception('train set err with unequal rank')
     pre_value = zeros((train_x.shape[1], 1))
     cur_value = ones((train_x.shape[1], 1))
     cnt = 0
-    while not (cur_value == pre_value).all():
+    is_nan = False
+    while (not (cur_value == pre_value).all()) and not is_nan:
         pre_value = cur_value.copy()
-        print('--------------- ' + str(cnt) + ' ---------------')
-        cnt += 1
         for col in range(train_x.shape[1]):
             # print('----- ' + str(col) + ' -----')
             # print('pre_value = ' + str(pre_value[col, 0]))
-            deta_h = (train_x * pre_value - train_y) * transpose(train_x)[col]
+            deta_h = transpose(train_x.dot(pre_value) - train_y).dot(train_x[:, col])
             # print('deta_h = \n' + str(deta_h))
-            deta_col = alpha / train_x.shape[0] * deta_h.sum()
+            deta_col = alpha / train_x.shape[0] * deta_h
             # print('deta_col = ' + str(deta_col))
             cur_value[col, 0] = pre_value[col, 0] + deta_col
             # print('cur_value = ' + str(cur_value[col][0]))
-        print('pre_value')
-        print(pre_value)
-        print('cur_value')
-        print(cur_value)
+            if 'nan' in str(cur_value[col, 0]) or 'inf' in str(cur_value[col, 0]):
+                is_nan = True
+                break
+        if show_detail:
+            print('--------------- ' + str(cnt) + ' ---------------')
+            print('pre_value')
+            print(pre_value)
+            print('cur_value')
+            print(cur_value)
+        cnt += 1
+    print('------- final result ------')
+    if is_nan:
+        print('!!!!!!! nan !!!!!!!')
+        print('!!!!!!! nan !!!!!!!')
+    print('----- count -----')
+    print(cnt)
+    if is_fs:
+        print('----- pre_x -----')
+        print(pre_x)
+    print('----- x -----')
+    print(train_x)
+    print('----- y -----')
+    print(train_y)
+    print('----- args -----')
+    print(cur_value)
 
 
-if __name__ == '__main__':
-    m = 10
-    alpha = -0.1
+def test1():
+    alpha = -0.01
+    train_x = array([[1, 1, 2], [1, 3, 4], [1, 5, 6]], dtype=float16)
+    train_y = array([[6], [12], [18]], dtype=float16)
+    gradient_descent(train_x, train_y, alpha, is_fs=True, show_detail=False)
+    gradient_descent(train_x, train_y, alpha, is_fs=False, show_detail=False)
+
+
+def test2():
+    m = 10000
+    alpha = -0.01
     train_x = []
     train_y = []
     for n in range(m):
-        a = random.randint(-10, 10)
+        a = random.randint(-100, 100)
         b = random.randint(-10, 10) * 123
-        c = random.random()
-        train_x.append((1, a, b, c))
-        noice = random.random() + 0.5
-        train_y.append((a * n + 2 * b + 3 * c * c) * noice) # 1000 +
+        c = random.randint(-10, 10)
+        train_x.append([1, a, b, c])
+        noice = (random.random() - 0.5) * 50
+        train_y.append([(1000 + a + 2 * b + 3 * c) + noice])
+    train_x = array(train_x, dtype=float64)
+    train_y = array(train_y, dtype=float64)
+    gradient_descent(train_x, train_y, alpha)
+    # gradient_descent(train_x, train_y, alpha * 0.1, False)
 
-    train_x = mat(train_x, dtype=float16)
-    train_y = transpose(mat(train_y, dtype=float16))
-    # print(train_x)
-    # print(train_y)
-    power_h = gradient_descent(train_x, train_y, alpha)
+
+if __name__ == '__main__':
+    # test1()
+    test2()
