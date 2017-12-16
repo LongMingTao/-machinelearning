1a4d279171d3f3be11a61188c6bdc055bd4bd78e
longmingtao <mingtao.long@hobot.cc>
2017-12-16 17:15:20 +0800

    add add test

--- linearegression.py
+++ linearegression.py
@@ -2,21 +2,25 @@ from numpy import *
 import random
 
 
-def feature_scaling(train_x, mode=None):
-    scaler = []
+def feature_scaling(train_x, scaler=None, mode=None):
     work_arr = train_x.copy()
     mode = 'mean_normalization' if not mode else mode
     if mode == 'mean_normalization':
-        for n in range(train_x.shape[1]):
-            col_s = train_x[:, n].max() - train_x[:, n].min()
-            col_avg = average(train_x[:, n])
-            scaler.append((col_avg, col_s))
-            if col_s == 0:
+        if not scaler:
+            scaler = []
+            for n in range(train_x.shape[1]):
+                col_s = train_x[:, n].max() - train_x[:, n].min()
+                col_avg = average(train_x[:, n])
+                scaler.append((col_avg, col_s))
+
+        for n in range(work_arr.shape[1]):
+            if scaler[n][1] == 0:
                 continue
             for i in range(work_arr.shape[0]):
-                work_arr[i, n] = (work_arr[i, n] - col_avg) / col_s
+                work_arr[i, n] = (work_arr[i, n] - scaler[n][0]) / scaler[n][1]
     else:
         pass
+
     return work_arr, scaler, mode
 
 
@@ -27,7 +31,7 @@ def gradient_descent(train_x, train_y, alpha, show_level=1):
     cur_value = ones((train_x.shape[1], 1))
     cnt = 0
     is_nan = False
-    while (not (cur_value == pre_value).all()) and not is_nan:
+    while (not (cur_value == pre_value).all()) and not is_nan and cnt <= 100000:
         pre_value = cur_value.copy()
         for col in range(train_x.shape[1]):
             deta_h = transpose(train_x.dot(pre_value) - train_y).dot(train_x[:, col])
@@ -58,9 +62,9 @@ def gradient_descent(train_x, train_y, alpha, show_level=1):
         print('----- linear regression result -----')
         print(cur_value)
     if is_nan:
-        return None
+        return None, cnt
     else:
-        return cur_value
+        return cur_value, cnt
 
 
 def create_train_set(m, n, ground_truth, x_ranges, noise_ranges, dtype=None):
@@ -76,7 +80,10 @@ def create_train_set(m, n, ground_truth, x_ranges, noise_ranges, dtype=None):
                 element = x_ranges[j]
             sum += ground_truth[j] * element
             elements.append(element)
-        sum += random.uniform(noise_ranges[0], noise_ranges[1])
+        if list == type(noise_ranges) or tuple == type(noise_ranges):
+            sum += random.uniform(noise_ranges[0], noise_ranges[1])
+        else:
+            sum += noise_ranges
         train_x.append(elements)
         train_y.append([sum])
     if dtype is None:
@@ -94,8 +101,13 @@ def test1():
     alpha = -0.01
     train_x = array([[1, 1, 2], [1, 3, 4], [1, 5, 6]])
     train_y = array([[6], [12], [18]], dtype=float16)
-    gradient_descent(train_x, train_y, alpha, is_fs=True, show_detail=False)
-    gradient_descent(train_x, train_y, alpha, is_fs=False, show_detail=False)
+    r1 = gradient_descent(train_x, train_y, alpha)
+    print('-----')
+    train_x, scaler, mode = feature_scaling(train_x)
+    r2 = gradient_descent(train_x, train_y, alpha)
+    print('----- scaler -----')
+    print(scaler)
+    return r1, r2
 
 
 def test2():
@@ -113,13 +125,31 @@ def test2():
     print(concatenate((train_x, train_y), axis=1))
     for a in alpha:
         print('===== alpha ' + str(a) + ' =====')
-        cur_value = gradient_descent(train_x, train_y, a, show_level=0)
+        cur_value, cnt = gradient_descent(train_x, train_y, a, show_level=0)
         if cur_value is not None:
             print('===== alpha ' + str(a) + ' SUCCESS =====')
             break
         else:
             print('===== alpha ' + str(a) + ' FAIL =====')
     print(cur_value)
+    test_x, test_y = create_train_set(2000, n, gt, x_range, 0)
+    pre_x = test_x.copy()
+    test_x = feature_scaling(test_x, scaler)[0]
+    print('----- test x,y -----')
+    print(concatenate((pre_x, test_y), axis=1))
+    result_y = test_x.dot(cur_value)
+    print('----- test x, result y -----')
+    print(concatenate((pre_x, result_y), axis=1))
+    print('----- parties of result_y - test_y -----')
+    deta = (result_y - test_y)[:, 0]
+    length = len(deta)
+    deta = [deta[i] * deta[i] for i in range(length)]
+    parties = 0
+    for i in range(length):
+        parties += deta[i]
+    parties /= length
+    print(parties)
+
 
 if __name__ == '__main__':
     # test1()
